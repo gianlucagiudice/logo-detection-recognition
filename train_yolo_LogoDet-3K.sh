7038e2e31e7289054788dda0d0fddff6c2c25aa3
gianlucagiudice <gianlu.giudice@gmail.com>
2022-03-22 00:18:19 +0100

    Train using LogoDet-3K normal

--- train_yolo_LogoDet-3K.sh
+++ train_yolo_LogoDet-3K.sh
@@ -1,2 +1,2 @@
-python download_LogoDet-3K.py --dataset_type small
-python yolov5/train.py --img 224 --batch 16 --epochs 50 --data LogoDet-3K_small.yaml --weights yolov5/yolov5s.pt
+python download_LogoDet-3K.py --dataset_type normal --sampling_fraction 0.25
+python yolov5/train.py --img 224 --batch 16 --epochs 50 --data LogoDet-3K_normal.yaml --weights yolov5/yolov5s.pt
