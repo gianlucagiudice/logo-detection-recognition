c9305e9140dac3076a7c43a068a8e90c3933c9a2
gianlucagiudice <gianlu.giudice@gmail.com>
2022-03-29 11:28:07 +0200

    Use the full dataset

--- train_yolo_LogoDet-3K.sh
+++ train_yolo_LogoDet-3K.sh
@@ -1,2 +1,2 @@
-python download_LogoDet-3K.py --dataset_type normal --sampling_fraction 0.25
+python download_LogoDet-3K.py --dataset_type normal --sampling_fraction 1
 python yolov5/train.py --img 224 --batch 16 --epochs 50 --data LogoDet-3K.yaml --weights yolov5/yolov5s.pt
