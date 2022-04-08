b36bef6e9a388bf212cbf18b620e338edb508c61
gianlucagiudice <gianlu.giudice@gmail.com>
2022-04-08 02:19:50 +0200

    Fix dataset sampling

--- train_yolo_LogoDet-3K.sh
+++ train_yolo_LogoDet-3K.sh
@@ -1,2 +1,2 @@
-python download_LogoDet-3K.py --dataset_type normal --sampling_fraction 1
-python yolov5/train.py --img 224 --batch 16 --epochs 50 --data LogoDet-3K.yaml --weights yolov5/yolov5s.pt
+python download_LogoDet-3K.py --dataset-type normal --sampling-fraction 1 --only-top --train-split 0.70 --validation-split 0.10 --test-split 0.20
+python yolov5/train.py --img 512 --batch 16 --epochs 50 --data LogoDet-3K.yaml --weights yolov5/yolov5m6.pt
