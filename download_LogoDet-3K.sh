a59bf80364e52312c3c7636523d849de9ab7443e
gianlucagiudice <gianlu.giudice@gmail.com>
2022-04-20 14:37:11 +0200

    Sample classes

--- download_LogoDet-3K.sh
+++ download_LogoDet-3K.sh
@@ -1,1 +1,1 @@
-python download_LogoDet-3K.py --dataset-type normal --sampling-fraction 1 --only-sample --train-split 0.70 --validation-split 0.10 --test-split 0.20 --sampling-method detection
+python download_LogoDet-3K.py --dataset-type normal --sampling-fraction 100 --only-sample --train-split 0.70 --validation-split 0.10 --test-split 0.20 --sampling-method cil
