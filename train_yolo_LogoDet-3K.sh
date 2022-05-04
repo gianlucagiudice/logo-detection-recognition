1a2fcbe59e9e12eefc58a0be421130d49c17ce65
gianlucagiudice <gianlu.giudice@gmail.com>
2022-05-05 01:35:11 +0200

    Delete file

--- train_yolo_LogoDet-3K.sh
+++
@@ -1,2 +0,0 @@
-python download_LogoDet-3K.py --dataset-type normal --sampling-fraction 1 --only-top --train-split 0.70 --validation-split 0.10 --test-split 0.20
-python yolov5/train.py --img 512 --batch 16 --epochs 50 --data LogoDet-3K.yaml --weights yolov5/yolov5m6.pt
