c83c37abf0edb9b8707aaafeedf87ecfcba722b4
gianlucagiudice <gianlu.giudice@gmail.com>
2022-05-19 22:54:58 +0200

    Full dataset

--- validate_detectors.sh
+++ validate_detectors.sh
@@ -1,10 +1,6 @@
 echo '----------------- Prepare Dataset -----------------'
-python train_yolo_4_cil.py --num-class 100 --no-start-training
+python train_yolo_4_cil.py --num-class 2993 --no-start-training --init-cls 1000 --increment-cls 250
 echo '------------------------ 1 ------------------------'
-python validate_detector.py --name yolo100_cil_classification --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolo-CIL-100cls.pt --cil-model-path weights/resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
+python validate_detector.py --name CIL_2993 --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolov5m6-CIL-512px-1000cls.pt --cil-model-path weights/CIL_1000_250_2993-mem100-resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
 echo '------------------------ 2 ------------------------'
-python validate_detector.py --name yolo100_agnostic --single-cls --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolo-CIL-100cls.pt --cil-model-path weights/resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
-echo '------------------------ 3 ------------------------'
-python validate_detector.py --name yolo30_cil_classification --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolo-CIL-30cls.pt --cil-model-path weights/resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
-echo '------------------------ 4 ------------------------'
-python validate_detector.py --name yolo30_yolo100_agnostic --single-cls --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolo-CIL-30cls.pt --cil-model-path weights/resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
+python validate_detector.py --name CIL_2993_agnostic --single-cls --data LogoDet-3K_CIL.yaml --yolo-model-path weights/yolov5m6-CIL-512px-1000cls.pt --cil-model-path weights/CIL_1000_250_2993-mem100-resnet34-pretrained-drop0.5-augmented-onlytop-adam.pt --task test --conf-thres 0.15
